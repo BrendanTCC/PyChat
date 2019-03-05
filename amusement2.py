d30a9a81b3ac1233c87f556b74965234db4cd072
ff3366 <47022364+ff3366@users.noreply.github.com>
2019-03-05 13:17:02 +1100

    cat rules the school
    Grouped response categories into lists, case doesn't matter when brendan enters his name, added more responses, program should capitalize names and responses when replying.

--- amusement2.py
+++ amusement2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import time
 import os
+import sys
+
+responsesGreat = ["great","awesome","rad","super cool","amazing"]
+responsesBad = ["bad","terrible","horrible","craptacular"]
+responsesGood = ["good","ok","okay","tiring","average","not great"]
+responsesGroovy = ["groovy","wild","freaky","trippy"]
 
 def intro():
     print("DISCLAIMER: this program has bugs. By using this program, you agree that if this program f**ks up your computer somehow, it's your own damned fault for not heeding the warning.")
@@ -21,24 +27,26 @@ def outro():
 
 def name(label):
     n = input(label)
-    if n == "Brendan":
+    if n.lower() == "brendan":
         day("Hello, boss!\nHow's your day been, boss? ", n)
+    elif n.lower() == "cat":
+        sys.exit("cat rules the school! slate")
     else:
-        day("Hello, " + n + ", how's your day been? ", n)
+        day("Hello, " + n.title() + ", how's your day been? ", n)
 
 def day(label, n):
     d = input(label)
-    if d.lower() == "great":
-        print(d + "? Oh golly," , n + "! That's amazing! Have a great day, " , n + "!")
+    if d.lower() in responsesGreat:
+        print(d.title() + "? Oh golly," , n.title() + "! That's amazing! Have a great day, " , n.title() + "!")
         time.sleep(1)
         print("EOF")
         time.sleep(3)
-    elif d.lower() == "bad":
-        print(d + "? " + d + "?! I wish i could help you bud, but I'm only a program.")
-    elif d.lower() == "good" or d.lower() == "tiring":
-        print("So your day isn't amazing, " + n + "? I wish I could help you with that, but I'm only a program.")
-    elif d.lower() == "groovy":
-        print("Far out, " + n)
+    elif d.lower() in responsesBad:
+        print(d.title() + "? " + d.title() + "?! I wish i could help you bud, but I'm only a program.")
+    elif d.lower() in responsesGood:
+        print("So your day isn't amazing, " + n.title() + "? I wish I could help you with that, but I'm only a program.")
+    elif d.lower() in responsesGroovy:
+        print("Far out, " + n.title())
     else:
         print("Wanker.")
 
